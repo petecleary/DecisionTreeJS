df4d10ece8ec86d17493b7af7f2288f5e9e62f71
Pete Cleary <pete.cleary@gmail.com>
2013-02-21 16:53:07 +0000

    Case

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,7 +3,7 @@ var navigation = {
         {text: "Home", href: "index.html"},
         {text: "Get started", href: "getting-started.html"},
         {text: "Documentation", href: "documentation.html"},
-        {text: "Examples", href: "examples/browser-simple/index.html"},
+        {text: "Examples", href: "examples/Browser-Simple/index.html"},
         {text: "Download", href: "download.html"}
     ],
     examplesBrowser: [
