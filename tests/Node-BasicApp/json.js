2596dfe7a95f1766f890f357f3c69bdd4ee24ec3
Pete Cleary <pete.cleary@gmail.com>
2013-02-21 16:17:25 +0000

    First Commit

---
+++ tests/Node-BasicApp/json.js
@@ -0,0 +1,41 @@
+﻿var shape = exports.shape = {
+    "shapeType": "Entry",
+    "id": 1,
+    "description": "Start",
+    "nextShape": {
+        "shapeType": "Operation",
+        "id": 2,
+        "description": "Adding value 2",
+        "nextShape": {
+            "shapeType": "Decision",
+            "id": 3,
+            "description": "Adding value 2",
+            "paths": [{
+                "value": true,
+                "selected": false,
+                "nextShape": null,
+                "nextShapeId": 3
+            },
+            {
+                "value": false,
+                "selected": false,
+                "nextShape":
+                    {
+                        "shapeType": "Operation",
+                        "id": 5,
+                        "description": "Adding nothing",
+                        "nextShape": null,
+                        "properties": { "value": 0 },
+                        "processName": "TestApp.add"
+                    },
+                "nextShapeId": 0
+            }],
+            "properties": { "value": 2, "decision": 10 },
+            "processName": "TestApp.add",
+            "nextShape": null,
+            "decideName": "TestApp.checkValue"
+        },
+        "properties": { "value": 2 },
+        "processName": "TestApp.add"
+    }
+};
---
+++ tests/Node-BasicApp/server.js
@@ -0,0 +1,9 @@
+﻿var TestApp = require("./app.js");
+var tree = require("./json.js");
+var decisionTree = require("../../lib/decisionTree.js");
+
+//create a data object to process
+var data = { "a": 1 };
+
+//execute the tree on the root shape
+decisionTree.execute(tree.shape, data, TestApp.complete, { "TestApp": TestApp });
